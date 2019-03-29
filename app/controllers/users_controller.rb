0a0317398d8f875e8ffb90905f0cd5bcabf51b40
michiharujp <michiharujp@icloud.com>
2019-03-29 19:25:47 +0900

    hensyu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
-  before_action :authentification, only[:show, :update, :destroy]
+  before_action :authentification, only: [:show, :update, :destroy]
 
   # GET /users
   def index
